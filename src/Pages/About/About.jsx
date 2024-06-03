35e7b2d2a632bb26db9acd28d2f3c266341b0a25
Divine <your-email@divinenation1.com>
2024-06-03 10:21:37 -0800

    all

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -21,10 +21,10 @@ const About = () => {
     <div>
       <div className="Abouthero min-h-screen px-4 py-32 flex items-center justify-center" data-aos="fade-up">
       <div className="text-center text-white p-8 abouthero-text space-y-4">
-  <h1 className="lg:text-7xl font-bold mb-4 aboutherotext">
+  <h1 className="font-bold mb-4 aboutherotext">
     A Reflection Of Our Collective Imagination
   </h1>
-  <p className="text-base md:text-xl mb-6 sectionAboutP">
+  <p className="text-base mb-6 sectionAboutP">
     With a legacy built on creativity, craftsmanship, and collaboration, we are dedicated to shaping spaces that transcend the ordinary and inspire the extraordinary.
   </p>
   <button className="btn mt-4">
